d9e9cf03293f876be87786fadfdae58a1c6a39b5
juansebastian-coder <sanchezjuansebastian51@gmail.com>
2021-01-17 11:34:30 -0500

    ajustes v1 simon dice

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,149 +1,184 @@
 var keys = [];
-var letrasSeleccionadas = [];
-var niveles = [];
-var puntaje = 0;
-var indicardorNivel = 1;
+var selectedLetters = [];
+var levels = [];
+var punctuation = 0;
+var LevelIndicator = 1;
 var play = false;
-
 //obtengo todos los botones del teclado
 keys = document.getElementsByClassName("key");
 
+
+
+
+//  estoy a la escucha de cualquier tecla para agregar
+//  su valor a la secuencia del usuario
 document.addEventListener("keydown", function (tecla) {
+
     let contain = null;
+
     for (let index = 0; index < keys.length; index++) {
         if (keys[index].dataset.key == tecla.keyCode) {
             contain = keys[index];
         }
     }
     if (play == true && contain) {
-        seleccionarTecla(contain);
+        selecttKey(contain);
     }
 });
 
-//  genero 10 niveles
-function generarRandomNivel() {
-    letrasSeleccionadas = [];
-    niveles = [];
-    indicardorNivel = 1;
-    puntaje = 0;
-    play = true;
 
-    document.getElementById("registrar").style.display = "none";
-    document.getElementById("nivel").innerHTML =
-        "<h6> Nivel :" + indicardorNivel + "</h6>";
-    document.getElementById("puntaje").innerHTML =
-        "<h6> Puntaje :" + puntaje + "</h6>";
+
+//  genero 10 niveles, o secuencias aleatorias
+function generateRandomLevel() {
+
+    selectedLetters = [];
+    levels = [];
+    LevelIndicator = 1;
+    punctuation = 0;
+    play = true;
+    document.getElementById('name').value = null;
+
+     document.getElementById("list-punctutation").innerHTML = '';
+
+
+    document.getElementById("register").style.display = "none";
+    document.getElementById("div-level").innerHTML =
+        "<h6> Nivel :" + LevelIndicator + "</h6>";
+    document.getElementById("div-punctuation").innerHTML =
+        "<h6> Puntaje :" + punctuation + "</h6>";
 
     for (let index = 0; index < keys.length; index++) {
         keys[index].classList.remove("active", "fail");
     }
 
-    for (let index = 0; index < 4; index++) {
-        niveles[index] = keys[getRandomArbitrary(25)];
-        console.log(niveles[index]);
+    for (let index = 0; index < 10; index++) {
+        levels[index] = keys[getRandomArbitrary(25)];
+        //console.log(levels[index]);
     }
 
     setTimeout(() => {
-        pintarTeclado(0);
+        paintKeyboard(0);
     }, 1000);
 }
 
- function seleccionarTecla(e) {
+function selecttKey(e) {
+
     //  verificar cuantas teclas puso bien el usuario
     let contador = 0;
+
     if (play) {
-        letrasSeleccionadas.push(e);
+        selectedLetters.push(e);
         e.classList.add("active");
-        setTimeout(function () {
-             e.classList.remove("active");
-            if (letrasSeleccionadas.length == indicardorNivel) {
+        setTimeout(() => {
+            e.classList.remove("active");
+            if (selectedLetters.length == LevelIndicator) {
                 for (
                     let index = 0;
-                    index < letrasSeleccionadas.length;
+                    index < selectedLetters.length;
                     index++
                 ) {
                     if (
-                        letrasSeleccionadas[index].dataset.key ==
-                        niveles[index].dataset.key
+                        selectedLetters[index].dataset.key ==
+                        levels[index].dataset.key
                     ) {
                         contador++;
-                    } 
+                    }
                 }
 
-                if (contador == indicardorNivel) {
-                    letrasSeleccionadas = [];
-                    indicardorNivel++;
-                    puntaje += 10;
+                if (contador == LevelIndicator) {
+                    selectedLetters = [];
+                    LevelIndicator++;
+                    punctuation += 10;
 
-                    var level = indicardorNivel;
 
-                    document.getElementById("nivel").innerHTML =
-                        "<h6> Nivel :" + indicardorNivel + "</h6>";
-                    document.getElementById("puntaje").innerHTML =
-                        "<h6> Puntaje :" + puntaje + "</h6>";
-
-                    for (let index = 0; index < indicardorNivel; index++) {
+                    document.getElementById("div-level").innerHTML =
+                        LevelIndicator == 11 ?
+                            "<h6> Nivel :" + 10 + "</h6>" : "<h6> Nivel :"+ LevelIndicator + "</h6>" ;
+                    document.getElementById("div-punctuation").innerHTML =
+                        "<h6> Puntaje :" + punctuation + "</h6>";
+                    var timeSet = 2000;
+                    for (let index = 0; index < LevelIndicator; index++) {
                         setTimeout(() => {
-                            pintarTeclado(index);
-                        }, 2000); 
+                            paintKeyboard(index);
+                        }, timeSet);
+                        timeSet += 1200
                     }
+
+
+
                 } else {
                     finishFailPlay();
                     alert("HAS PERDIDO, VUELVE A INTENTARLO 😢");
                 }
 
-                if (indicardorNivel == 5) {
+                if (LevelIndicator == 11) {
                     finishSuccessPlay();
                     alert("Felicidades has ganado 😃");
                 }
-            } else if (letrasSeleccionadas.length > indicardorNivel) {
+            } else if (selectedLetters.length > LevelIndicator) {
                 finishFailPlay();
                 console.log("aqui");
                 alert("HAS PERDIDO, VUELVE A INTENTARLO 😢");
             }
-        }, 1500);
+        }, 1000);
     }
 }
 
-// cambio el valor del background
- function pintarTeclado(index) {
-    niveles[index].classList.add("active", "success");
-         setTimeout(function () {
-        niveles[index].classList.remove("active", "success");
-    }, 2000);
+
+// pinta los botones del teclado en pantalla
+function paintKeyboard(index) {
+    levels[index].classList.add("active", "success");
+    setTimeout(() => {
+        levels[index].classList.remove("active", "success");
+    }, 900);
 }
+
 
 // Retorna un número aleatorio entre min (incluido) y max (excluido)
 function getRandomArbitrary(max) {
     return Math.round(Math.random() * max);
 }
 
+
+// se agrega clase fail , color rojo que idica fallo
 function setFailColor(iterator) {
     keys[iterator].classList.add("active", "fail");
 }
 
+
+
+// se agrega clase success , color verde que idica exito
 function setSuccessColor(iterator) {
     keys[iterator].classList.add("active", "success");
 }
 
+
+//  finaliza el proceso de manera fallida
 function finishFailPlay() {
     play = false;
     for (let iterator = 0; iterator < keys.length; iterator++) {
         setFailColor(iterator);
     }
-    letrasSeleccionadas = [];
-    document.getElementById("registrar").style.display = "block";
+    selectedLetters = [];
+    document.getElementById("register").style.display = "block";
 }
 
+
+//  finaliza el proceso de manera exitosa
 function finishSuccessPlay() {
     play = false;
     for (let iterator = 0; iterator < keys.length; iterator++) {
         setSuccessColor(iterator);
     }
-    letrasSeleccionadas = [];
-    document.getElementById("registrar").style.display = "block";
+    selectedLetters = [];
+    document.getElementById("register").style.display = "block";
 }
 
+
+//abre la modal de registro de datos
 function showModal() {
+
+    document.getElementById("punctuation").value = punctuation;
+    document.getElementById("level").value = LevelIndicator - 1;
     document.getElementById("exampleModal").style.display = "block";
 }
--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,88 +1,131 @@
-<!-- Modal -->
-<div style="display: none;" class="modal" id="exampleModal" >
+<div style="display: none" class="modal" id="exampleModal">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Guardar Resultado</h5>
+                <h5 class="modal-title text-black" id="exampleModalLabel">
+                    Guardar Resultado
+                </h5>
                 <button type="button" class="close" onclick="hideModal()">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form>
-                    <label class="text-black" for="name">Nombre Participante</label>
-                    <input type="text" id="name" class="form-control" placeholder="Ingresa tu nombre">
-                    <input type="hidden" id="token" value="{{csrf_token()}}">
+                    <div class="form-group">
+                        <label class="text-black" for="name"
+                            >Nombre Participante</label
+                        >
+                        <input
+                            type="text"
+                            id="name"
+                            class="form-control"
+                            placeholder="Ingresa tu nombre"
+                        />
+                        <input
+                            type="hidden"
+                            id="token"
+                            value="{{ csrf_token() }}"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label class="text-black" for="punctuation"
+                            >Puntaje</label
+                        >
+                        <input
+                            type="text"
+                            id="punctuation"
+                            class="form-control"
+                            disabled
+                        />
+                    </div>
 
+                    <div class="form-group">
+                        <label class="text-black" for="level"
+                            >Niveles Acertados</label
+                        >
+                        <input
+                            type="text"
+                            id="level"
+                            class="form-control"
+                            disabled
+                        />
+                    </div>
                 </form>
-
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" onclick="hideModal()" >Cerrar</button>
-                <button type="submit" required class="btn btn-primary" onclick="registrar() ">Guardar</button>
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    onclick="hideModal()"
+                >
+                    Cerrar
+                </button>
+                <button
+                    type="submit"
+                    required
+                    class="btn btn-primary"
+                    onclick="register() "
+                >
+                    Guardar
+                </button>
             </div>
         </div>
     </div>
 </div>
 
-
-
 <script>
-    function registrar() {
-        if(document.getElementById('name').value!='' &&
-        document.getElementById('name').value!=null){
-
-       
-        fetch("{{url('/register-game')}}", {
-            headers: {
-                'X-CSRF-TOKEN': document.getElementById('token').getAttribute('value'),
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                name: document.getElementById('name').value,
-                punctuation: puntaje,
-                level: indicardorNivel,
+    function register() {
+        // envio hecho con Fetch, registra los datos del cliente, puntaje y nivel
+        if (
+            document.getElementById("name").value != "" &&
+            document.getElementById("name").value != null
+        ) {
+            fetch("{{url('/register-game')}}", {
+                headers: {
+                    "X-CSRF-TOKEN": document
+                        .getElementById("token")
+                        .getAttribute("value"),
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    name: document.getElementById("name").value,
+                    punctuation: punctuation,
+                    level: LevelIndicator - 1,
+                }),
             })
-        })
-            .then(function (response) {
-                if (response.ok) {
-                    return response.text()
-                } else {
-                    throw "Error en la llamada Ajax";
-                }
-
-            })
-            .then(function (texto) {
-                console.log(texto);
-                hideModal();
-                document.getElementById('list-punctutation').innerHTML= texto;
-                window.location.href = '#list-punctutation';
-             })
-            .catch(function (err) {
-                console.log(err);
-                hideModal();
-            });
-
-         }else{
-             alert('TÚ NOMBRE ES NECESARIO PARA EL REGISTRO 😊');
-         }
+                .then(function (response) {
+                    if (response.ok) {
+                        return response.text();
+                    } else {
+                        throw "Error en la llamada Ajax";
+                    }
+                })
+                .then(function (data) {
+                    hideModal();
+                    document.getElementById(
+                        "list-punctutation"
+                    ).innerHTML = data;
+                    window.location.href = "#list-punctutation";
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    hideModal();
+                });
+        } else {
+            alert("TÚ NOMBRE ES NECESARIO PARA EL REGISTRO 😊");
+        }
     }
 
-
-
-    function hideModal(){
-             document.getElementById('exampleModal').style.display = 'none'
-            
+    //  cierra la modal
+    function hideModal() {
+        document.getElementById("exampleModal").style.display = "none";
     }
 </script>
-
-
 
 <style>
     .text-black {
         color: black;
         font-weight: bold;
-
     }
 </style>
